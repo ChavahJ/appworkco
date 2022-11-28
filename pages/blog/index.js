3bf9dd5eb7ed0b2976198258c8e99617603c9972
Chavah Jacobs <chavah.jacobs@gmail.com>
2022-11-28 14:08:05 +0200

    adding tailwind css and fonts

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,21 @@
 import styles from "../../styles/Home.module.css";
+import Link from "next/link";
 
 function BlogPostsPage() {
+  const blogs = [
+    { id: "launch", name: "AppWork Launches" },
+    { id: "bert", name: "Meet Bert" },
+  ];
   return (
     <main className={styles.main}>
       <h1>AppWork Blog Posts -- Category Page</h1>
+      {blogs.map((blog) => (
+        <li key={blogs.id}>
+          <Link href={{ pathname: "/blog/[slug]", query: { slug: blog.id } }}>
+            {blog.name}
+          </Link>
+        </li>
+      ))}
     </main>
   );
 }
