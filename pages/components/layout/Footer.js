77cc1a53fb871c41227497617fc6fa565dfd56d0
Chavah Jacobs <chavah.jacobs@gmail.com>
2022-12-11 22:20:49 +0200

    feature(footer): working on mobile viewport

--- pages/components/layout/Footer.js
+++ pages/components/layout/Footer.js
@@ -9,10 +9,14 @@ import TwitterIcon from "../icons/Twitter";
 import YouTubeIcon from "../icons/YouTube";
 
 function Footer() {
+  const today = new Date();
+  const year = today.getFullYear();
+
   return (
     <footer className="flex w-full flex-col bg-forest-500 font-light text-green-200">
-      <div className="container mx-auto flex w-screen flex-wrap justify-between pt-6 pb-4">
-        <div className="flex flex-col items-center gap-4">
+      <div className="container mx-auto flex w-screen flex-wrap justify-around px-6 pt-6 pb-4">
+        <hr className="border-b-1 my-5 w-screen border-green-700 opacity-25 md:hidden" />
+        <div className="flex w-2/5 flex-col items-center gap-4">
           <Link href="/">
             <Image
               className="p-2"
@@ -28,65 +32,87 @@ function Footer() {
             <p>Greensboro, NC 27406</p>
           </div>
         </div>
-        <div className="flex justify-around align-bottom">
-          <div>
-            <p className="mb-3 text-lg font-normal text-white">Company</p>
-            <ul className="flex list-none flex-col">
-              <li>
-                <Link href="/features">Product Features</Link>
-              </li>
-              <li>
-                <Link href="/about-us">Our Company</Link>
-              </li>
-              <li>
-                <Link href="/advantages">Our Advantages</Link>
-              </li>
-              <li>
-                <Link href="/meet-the-team">Meet the Team</Link>
-              </li>
-              <li>
-                <Link href="/proptech-news">PropTech News</Link>
-              </li>
-              <li>
-                <Link href="/privacy">Privacy Policy</Link>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <p className="mb-3 text-lg font-normal text-white">Follow Us On</p>
-            <ul className="flex list-none">
-              <li className="footer-icon">
-                <LinkedInIcon />
-              </li>
-              <li className="footer-icon">
-                <InstagramIcon />
-              </li>
-              <li className="footer-icon">
-                <FacebookIcon />
-              </li>
-              <li className="footer-icon">
-                <TwitterIcon />
-              </li>
-              <li className="footer-icon">
-                <YouTubeIcon />
-              </li>
-            </ul>
-          </div>
-          <div>
-            <p className="mb-3 text-lg font-normal text-white">Available On</p>
-            <ul className="flex list-none">
-              <li className="footer-icon">
-                <AppleIcon />
-              </li>
-              <li className="footer-icon">
-                <PlayStoreIcon />
-              </li>
-            </ul>
-          </div>
+        <div className="order-2 mr-auto mt-6 md:order-1">
+          <p className="mb-3 hidden text-lg font-normal text-white sm:block">
+            Company
+          </p>
+          <ul className="flex list-none flex-col text-sm md:text-base">
+            <li>
+              <Link href="/features">Product Features</Link>
+            </li>
+            <li>
+              <Link href="/about-us">Our Company</Link>
+            </li>
+            <li>
+              <Link href="/advantages">Our Advantages</Link>
+            </li>
+            <li>
+              <Link href="/meet-the-team">Meet the Team</Link>
+            </li>
+            <li>
+              <Link href="/proptech-news">PropTech News</Link>
+            </li>
+            <li>
+              <Link href="/privacy">Privacy Policy</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="order-1 w-3/5 pl-2 md:order-2">
+          <p className="mb-3 hidden text-lg font-normal text-white sm:block">
+            Contact Us
+          </p>
+          <ul className="flex list-none flex-col text-sm md:text-base">
+            <li>
+              <Link href="tel:18888581449">+1 888-858-1449</Link>
+            </li>
+            <li>
+              <Link href="mailto:sales@appworkco.com">sales@appworkco.com</Link>
+            </li>
+            <li>
+              <Link href="mailto:support@appworkco.com">
+                support@appworkco.com
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="order-first md:order-last">
+          <p className="mb-3 hidden text-lg font-normal text-white sm:block">
+            Follow Us On
+          </p>
+          <ul className="flex list-none">
+            <li className="footer-icon">
+              <LinkedInIcon />
+            </li>
+            <li className="footer-icon">
+              <InstagramIcon />
+            </li>
+            <li className="footer-icon">
+              <FacebookIcon />
+            </li>
+            <li className="footer-icon">
+              <TwitterIcon />
+            </li>
+            <li className="footer-icon">
+              <YouTubeIcon />
+            </li>
+          </ul>
+        </div>
+        <div className="order-first md:order-last">
+          <p className="mb-3 hidden text-lg font-normal text-white sm:block">
+            Available On
+          </p>
+          <ul className="flex list-none">
+            <li className="footer-icon">
+              <AppleIcon />
+            </li>
+            <li className="footer-icon">
+              <PlayStoreIcon />
+            </li>
+          </ul>
         </div>
       </div>
-      <div className="container mx-auto pt-8 pb-4 text-sm">
-        © 2022 AppWork. All rights reserved.
+      <div className="container mx-auto px-6 py-4 text-sm">
+        © {year} AppWork. All rights reserved.
       </div>
     </footer>
   );
