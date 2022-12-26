772ecfa8a33b135833f8a909a759b39c9f6e7e09
Chavah Jacobs <chavah.jacobs@gmail.com>
2022-12-26 23:16:21 +0200

    feature(strapi): working on fetching data from strapi and parsing it

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,24 +1,71 @@
 import { useRouter } from "next/router";
 import { Fragment } from "react";
-import styles from "../../styles/Home.module.css";
 
-function BlogPostPage() {
-  const router = useRouter();
+function BlogPostPage(props) {
+  //const router = useRouter();
 
-  console.log(router.pathname);
-  console.log(router.query.slug);
+  // console.log(router.pathname);
+  // console.log(router.query.slug);
 
   // send a request to backend server
   // fetch piece of data with slug of router.query.slug
-  // if (!blog) {
-  //   return <p>No blog post found!</p>;
-  // }
+
+  const { blog } = props;
 
   return (
-    <main>
-      <h1>AppWork Blog Post--Single Page</h1>
-    </main>
+    <Fragment>
+      <main>
+        <h1>{blog.attributes.title}</h1>
+        <p>{blog.attributes.description}</p>
+      </main>
+    </Fragment>
   );
 }
 
+async function getData() {
+  const myRequest = new Request("http://localhost:1337/api/blogs");
+  const resp = await fetch(myRequest)
+    .then((response) => response.json())
+    .catch(console.error);
+
+  return resp;
+}
+export async function getStaticProps(context) {
+  const { params } = context;
+  const blogSlug = params.slug;
+
+  const resp = await getData();
+
+  if (!resp) {
+    return {
+      redirect: {
+        destination: "/",
+      },
+    };
+  }
+  const blog = resp.data.find((blog) => blog.attributes.slug === blogSlug);
+
+  if (!blog) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      blog,
+    },
+    revalidate: 60,
+  };
+}
+
+export async function getStaticPaths() {
+  const resp = await getData();
+  const attributes = resp.data.map((blog) => blog.attributes);
+  const attributeArray = Object.values(attributes);
+  const slugs = attributeArray.map((attribute) => attribute.slug);
+  const pathsWithParams = slugs.map((slug) => ({ params: { slug: slug } }));
+  return {
+    paths: pathsWithParams,
+    fallback: true,
+  };
+}
 export default BlogPostPage;
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,23 +1,46 @@
 import styles from "../../styles/Home.module.css";
 import Link from "next/link";
 
-function BlogPostsPage() {
-  const blogs = [
-    { id: "launch", name: "AppWork Launches" },
-    { id: "bert", name: "Meet Bert" },
-  ];
+function BlogPostsPage(props) {
+  const { blogs } = props;
+
   return (
     <main className={styles.main}>
       <h1>AppWork Blog Posts -- Category Page</h1>
       {blogs.map((blog) => (
-        <li key={blogs.id}>
-          <Link href={{ pathname: "/blog/[slug]", query: { slug: blog.id } }}>
-            {blog.name}
+        <li key={blog.id}>
+          <Link href={`/blog/${encodeURIComponent(blog.attributes.slug)}`}>
+            {blog.attributes.title}
           </Link>
         </li>
       ))}
     </main>
   );
+}
+
+export async function getStaticProps(context) {
+  const myRequest = new Request("http://localhost:1337/api/blogs");
+  const resp = await fetch(myRequest)
+    .then((response) => response.json())
+    .catch(console.error);
+
+  if (!resp) {
+    return {
+      redirect: {
+        destination: "/",
+      },
+    };
+  }
+  if (resp.data.length === 0) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      blogs: resp.data,
+    },
+    revalidate: 60,
+  };
 }
 
 export default BlogPostsPage;
